c4ad9018800f2b048e1125646c22192a734e9072
wyff1999123 <1075822210@qq.com>
2020-01-03 17:05:58 +0800

    员工管理，顾客管理

--- src/pages/customer/List.vue
+++ src/pages/customer/List.vue
@@ -19,7 +19,7 @@
     </el-table>
     <!-- /表格结束 -->
     <!-- 分页开始 -->
-    <el-pagination layout="prev, pager, next" :total="50"></el-pagination>
+    <!-- <el-pagination layout="prev, pager, next" :total="50"></el-pagination> -->
     <!-- /分页结束 -->
     <!-- 模态框 -->
     <el-dialog
